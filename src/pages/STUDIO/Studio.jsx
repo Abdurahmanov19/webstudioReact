50335b740879e2d68a4e9ede6472aa592ac64db1
nurbolot20 <videos22333@gmail.com>
2026-09-16 18:18:41 +0600

    Сделал адаптацию advantes

--- src/pages/STUDIO/Studio.jsx
+++ src/pages/STUDIO/Studio.jsx
@@ -39,63 +39,63 @@ const Studio = () => {
     const [active, setActive] = useState(false);
     return (
         <div>
-            {/*<section className='hero'>*/}
-            {/*    <div className="container">*/}
-            {/*        <h2 className="hero__title">Эффективные решения <br/>*/}
-            {/*            для вашего бизнеса</h2>*/}
-            {/*        <button onClick={() => setActive(prev => !prev)} className="hero__btn">Заказать услугу</button>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-            {/*<section className='advantages'>*/}
-            {/*    <div className="container">*/}
-            {/*        <div className="advantages__block">*/}
-            {/*            <div className="advantages__box">*/}
-            {/*                <div className="advantages__content">*/}
-            {/*                    <img src="./../../../public/advantages-antenna.svg" alt="" className="advantages__img"/>*/}
-            {/*                </div>*/}
-            {/*                <h4 className="advantages__title">Внимание к деталям</h4>*/}
-            {/*                <p className="advantages__text">*/}
-            {/*                    Идейные соображения, а также <br/>*/}
-            {/*                    начало повседневной работы по <br/>*/}
-            {/*                    формированию позиции.*/}
-            {/*                </p>*/}
-            {/*            </div>*/}
-            {/*            <div className="advantages__box">*/}
-            {/*                <div className="advantages__content">*/}
-            {/*                    <img src="./../../../public/advantages-clock.svg" alt="" className="advantages__img"/>*/}
-            {/*                </div>*/}
-            {/*                <h4 className="advantages__title">Пунктуальность</h4>*/}
-            {/*                <p className="advantages__text">*/}
-            {/*                    Задача организации, в особенности <br/>*/}
-            {/*                    же рамки и место обучения кадров <br/>*/}
-            {/*                    влечет за собой.*/}
-            {/*                </p>*/}
-            {/*            </div>*/}
-            {/*            <div className="advantages__box">*/}
-            {/*                <div className="advantages__content">*/}
-            {/*                    <img src="./../../../public/advantages-diagram.svg" alt="" className="advantages__img"/>*/}
-            {/*                </div>*/}
-            {/*                <h4 className="advantages__title">Планирование</h4>*/}
-            {/*                <p className="advantages__text">*/}
-            {/*                    Равным образом консультация с <br/>*/}
-            {/*                    широким активом в значительной <br/>*/}
-            {/*                    степени обуславливает.*/}
-            {/*                </p>*/}
-            {/*            </div>*/}
-            {/*            <div className="advantages__box">*/}
-            {/*                <div className="advantages__content">*/}
-            {/*                    <img src="./../../../public/advantages-astronaut.svg" alt="" className="advantages__img"/>*/}
-            {/*                </div>*/}
-            {/*                <h4 className="advantages__title">Современные технологии</h4>*/}
-            {/*                <p className="advantages__text">*/}
-            {/*                    Значимость этих проблем настолько <br/>*/}
-            {/*                    очевидна, что реализация плановых <br/>*/}
-            {/*                    заданий.*/}
-            {/*                </p>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
+            <section className='hero'>
+                <div className="container">
+                    <h2 className="hero__title">Эффективные решения <br/>
+                        для вашего бизнеса</h2>
+                    <button onClick={() => setActive(prev => !prev)} className="hero__btn">Заказать услугу</button>
+                </div>
+            </section>
+            <section className='advantages'>
+                <div className="container">
+                    <div className="advantages__block">
+                        <div className="advantages__box">
+                            <div className="advantages__content">
+                                <img src="./../../../public/advantages-antenna.svg" alt="" className="advantages__img"/>
+                            </div>
+                            <h4 className="advantages__title">Внимание к деталям</h4>
+                            <p className="advantages__text">
+                                Идейные соображения, а также <br/>
+                                начало повседневной работы по <br/>
+                                формированию позиции.
+                            </p>
+                        </div>
+                        <div className="advantages__box">
+                            <div className="advantages__content">
+                                <img src="./../../../public/advantages-clock.svg" alt="" className="advantages__img"/>
+                            </div>
+                            <h4 className="advantages__title">Пунктуальность</h4>
+                            <p className="advantages__text">
+                                Задача организации, в особенности <br/>
+                                же рамки и место обучения кадров <br/>
+                                влечет за собой.
+                            </p>
+                        </div>
+                        <div className="advantages__box">
+                            <div className="advantages__content">
+                                <img src="./../../../public/advantages-diagram.svg" alt="" className="advantages__img"/>
+                            </div>
+                            <h4 className="advantages__title">Планирование</h4>
+                            <p className="advantages__text">
+                                Равным образом консультация с <br/>
+                                широким активом в значительной <br/>
+                                степени обуславливает.
+                            </p>
+                        </div>
+                        <div className="advantages__box">
+                            <div className="advantages__content">
+                                <img src="./../../../public/advantages-astronaut.svg" alt="" className="advantages__img"/>
+                            </div>
+                            <h4 className="advantages__title">Современные технологии</h4>
+                            <p className="advantages__text">
+                                Значимость этих проблем настолько <br/>
+                                очевидна, что реализация плановых <br/>
+                                заданий.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
             {/*<section className="service">*/}
             {/*    <div className="container">*/}
             {/*        <h2 className="service__title">Чем мы занимаемся</h2>*/}
